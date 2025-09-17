86d2c980fe3943b5a7b787f5d1f9ee7ddf9eaf70
dannyednut <dannyednut88@gmail.com>
2025-09-17 15:59:43 +0100

    nought

--- engine.py
+++ engine.py
@@ -251,7 +251,7 @@ class Engine:
         try:
             # --- Build unique opportunity signature ---
             if entity_name == 'ArbitrageOpportunity':
-                signature = f"{op.trading_pair}-{op.buy_exchange}-{op.sell_exchange}-{round(op.profit_percentage, 0)}"
+                signature = f"{op.trading_pair}-{op.buy_exchange}-{op.sell_exchange}-{round(op.profit_percentage, 1)}"
             elif entity_name == 'TriangularOpportunity':
                 signature = f"{op.exchange}-{'->'.join(op.trading_path)}-{round(op.profit_percentage, 1)}"
             else:
